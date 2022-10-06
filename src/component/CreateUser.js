152816020378fe5f582b43b5f2dce12e78eff054
omarfarids <omar.farid007@gmail.com>
2022-10-06 22:52:56 +0200

    updates in UI

--- src/component/CreateUser.js
+++ src/component/CreateUser.js
@@ -7,7 +7,7 @@ function CreateUser() {
     const {user , setUser ,createNewUser, setSignup  ,sender ,setSender} = useContext(AppContext)
 
   return (
-    <form  className={styles.signForm} onSubmit={createNewUser}>
+    <form className={styles.signForm + ' dark'} onSubmit={createNewUser}>
         <input type='text' value={sender} placeholder='username' onChange={(e)=>setSender(e.target.value)} required/>
         <input type='text' value={user.userID} placeholder='userID' onChange={(e)=>setUser({...user,userID:e.target.value})} required  minLength='8' maxLength='40'/>
         <input type='password' value={user.password} placeholder='password' onChange={(e)=>setUser({...user,password:e.target.value})} required  minLength='8' maxLength='40'/>
